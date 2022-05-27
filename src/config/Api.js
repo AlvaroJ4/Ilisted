f06eb84742ecfc5a42377665ef25ec28f74b8f44
AlvaroJ4 <alvaro.pereiraJ4@gmail.com>
2022-05-27 15:22:22 -0300

    master

---
+++ src/config/Api.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase/app';
+//import 'firebase/compat/auth';
+//import 'firebase/compat/firestore';
+
+//import firebaseConfig from "./firebase";
+
+//const firebaseApp = firebase.initializeApp(firebaseConfig);
+//const db = firebaseApp.firestore();
+
+//export default {
+
+  //  googleLogar: async () => {
+    //    const provider = new firebase.auth.GoogleAuthProvider();
+      //  let result = await firebase.auth().singInWithPopup(provider)
+        //return result;
+        
+   // }
+//}
